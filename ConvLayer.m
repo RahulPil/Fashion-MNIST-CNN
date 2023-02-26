870918e20951132f3fcd0626b3b7fb7c79c4aef2
hhhults <hhhults@uw.edu>
2023-02-26 14:42:47 -0800

    fixed forward

--- ConvLayer.m
+++ ConvLayer.m
@@ -14,7 +14,7 @@ classdef ConvLayer < Layer
         end
 
         function output = forward(obj, input)
-            output = zeros(size(obj.weightMatrix),obj.numFilters);
+            output = zeros(size(input,1)-size(obj.weightMatrix,1)+1,size(input,2)-size(obj.weightMatrix,2)+1,obj.numFilters);
             for i =1:obj.numFilters
                 output(:,:,i) = transfer(conv2(input,obj.weightMatrix(:,:,i)));
             end
