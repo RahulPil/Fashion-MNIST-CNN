b4ba1e3c68d474035e7f2b7790bde2163974d0d4
Rahul Pillalamarri <100654729+RahulPil@users.noreply.github.com>
2023-03-05 04:16:36 -0800

    Update ConvLayer.m
    added to updateLayer and calcSensitivity functions

--- ConvLayer.m
+++ ConvLayer.m
@@ -21,6 +21,30 @@ classdef ConvLayer < Layer
             obj.lastInput = input;
         end
 
-        % tfw conv backprop is complicated ;-;
+        function [obj, s] = calcSensitivity(obj, prevSensitivity)
+            % this is the one where we have a 180 flipped kernel that
+            % convolves over the gradient recieved from the maxpool layer
+            % but make sure that its a full convolution
+            % do we also have to actually use the direvative of the relu
+            % function here? because we could but... idk i thought it might
+            % be redundant but it might not be if we arent dealing with any
+            % max pooling layers in the network right?
+            s = conv2(prevSensitivity, rot90(obj.weightMatrix, 2), 'full');
+            obj.sensitivity = s;
+        end
+        % in hindsight I thnk that this function should do the same thing
+        % as updatelayer1 but im not sure if we wanna keep this or not
+        function obj = updateLayer(obj)
+        end
+
+        function obj = updateLayer1(obj, prevSensitivity, output)
+            % this is the one where we update the weights by convolving the
+            % sensitivity of the preivous layer over the input image and
+            % then do the regular ting for updating the weights
+            obj.weightMatrix = obj.weightMatrix - obj.learningRate*(conv2(output, prevSensitivity));
+            
+            % updating the bias vector is slightly confusing. bias vectors
+            % are overated anyway who tf even cares about them 
+        end
     end
 end
