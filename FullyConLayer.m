fc66b800df1c3a8a958344972e4e4754d1cd90fc
Rahul Pillalamarri <100654729+RahulPil@users.noreply.github.com>
2023-03-05 04:17:29 -0800

    Update FullyConLayer.m
    added to calcSensitivty and updateLayer functions

--- FullyConLayer.m
+++ FullyConLayer.m
@@ -1,6 +1,9 @@
 classdef FullyConLayer < Layer
     %UNTITLED5 Summary of this class goes here
     %   Detailed explanation goes here
+    properties
+        sensitivity
+    end
 
     methods
         function obj = FullyConLayer(inputSize, outputSize, transfer)
@@ -9,16 +12,20 @@ classdef FullyConLayer < Layer
 
         function [obj, output] = forward(obj,input)
             output = obj.transfer(obj.weightMatrix*input+obj.biasVector);
-            obj.lastInput = input;
+        end
+        
+        %i just dont know the direvative of the softmax function but once i
+        %get that i think all we have to do is simply replace the (1-a) .*a
+        %with the direvative of the softmax function cause the prior is the
+        %direvative of the logsigmoid function
+        function obj = calcSensitivity(obj, error, a)
+            s = -2*diag((1 - a) .* a) * e;
+            obj.sensitivity = s;
         end
 
-        function obj = calcSensitivity(obj, prevOut, nextSens, nextWeight) % do we need to store net input?
-        end
-
-        function obj = calcLastSensitivity(obj, error, output)
-        end
-
-        function obj = updateLayer(obj, sensitivity, prevOut)
+        function obj = updateLayer(obj, prevOutput)
+            obj.weightMatrix = obj.weightMatrix - obj.learningRate*obj.sensitivity*prevOutput';
+            obj.biasVector = obj.biasVector - obj.learningRate*obj.sensitivity;
         end
     end
 end
