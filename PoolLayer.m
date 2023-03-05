508b835e3715d16179e6c60626ba539293c13d13
Rahul Pillalamarri <100654729+RahulPil@users.noreply.github.com>
2023-03-05 04:13:29 -0800

    Update PoolLayer.m
    added the calcSensitivity function

--- PoolLayer.m
+++ PoolLayer.m
@@ -1,11 +1,12 @@
 classdef PoolLayer < Layer
-    %UNTITLED4 Summary of this class goes here
-    %   Detailed explanation goes here
 
     properties
         inputSize
         outputSize
         stride
+        lastInput
+        % newly created property for calculating the sensitivity
+        output
     end
 
     methods
@@ -14,27 +15,73 @@ classdef PoolLayer < Layer
             obj.outputSize = [inputSize(1)/stride inputSize(2)/stride];
             obj.stride = stride;
         end
-
+        
+        % only thing i modifed was creating a new property called output
+        % and setting the output on line 33 to obj,output instead of just
+        % output. Reason for this was because I needed to know which values
+        % were chosen for the max values
         function [obj, output] = forward(obj,input)
             output = zeros(obj.outputSize);
+            % you sure u dont mean obj.outputSize(2) for the columns
+            % because obj.outputSize(0) isnt a valid index
             for row = 1:obj.outputSize(1)
                 for col = 1: obj.outputSize(0)
                     i = (row-1)*obj.stride;
                     j = (col-1)*obj.stride;
                     % need to somehow store which ones we are picking
-                    output(row,col) = max(input(i+1:i+obj.stride,j+1:j+obj.stride));
+                    obj.output(row,col) = max(input(i+1:i+obj.stride,j+1:j+obj.stride));
                 end
             end
             obj.lastInput = input;
         end
-
-        function obj = calcSensitivity(obj, prevOut, nextSens, nextWeight) % do we need to store net input?
-            % it's grad of next layer but only on the elements that were
-            % max
+        
+        %ngl this was a bitch and a half to figure out not hard just
+        %annoying as fuck for some reason. Idk
+        % i think this code should work... hopefully
+        function s = calcSensitivity(obj)
+            % saves the coordinates of the max values in the lastInput
+            % matrix
+              max_val_coords = [];
+              
+              % similar for loops that was used in the forward function. 
+              for row=1:obj.outputSize(1)
+                  for col=1:obj.outputSize(2)
+                      i = (row-1)*obj.stride;
+                      j = (col-1)*obj.stride;
+                      % creates a window with the proper size that we want to search
+                      % although i
+                      % realize now that we might want to do it with
+                      % respect to the outputSize(1) and outputSize(2)
+                      % rather than obj.stride
+                      % if it works then dope otherwise maybe do that
+                      % change
+                      window = obj.lastInput(i+1:i+obj.stride,j+1:j+obj.stride);
+                      % if the max value of the current stride is in the window that we are
+                      % currently looking at
+                      if any(window(:) == obj.output(row, col))
+                          % append the coordinates (row, col) of the max
+                          % value in the corrosponding output row and col
+                          % using the find function. The function is going
+                          % to return the first occurance of the value in
+                          % case of repeat values in the window
+                          coords = find(window == obj.output(row, col), 1, "first")
+                          max_val_coords = [max_val_coords:coords];
+                      end
+                  end
+              end
+              % basically create a matrix x that has a logical index of
+              % which elements to keep or not so that all other values can
+              % be set to 0 except for the specific coordinates of the max
+              % values
+              x = false(size(obj.lastInput));
+              x(sub2ind(size(obj.lastInput), max_val_coords(:,1), max_val_coords(:,2))) = true;
+              % setting all values that are not true to 0
+              s(~x)=0;                           
         end
-
-        function obj = updateLayer(obj, sensitivity, prevOut)
-            % idfk
+        % purely only here because if we dont have this and the following
+        % functions then this technically isnt a subclass but an abstract
+        % class so it wont be able to fall under the layer class
+        function obj = updateLayer(obj)
         end
     end
 end
